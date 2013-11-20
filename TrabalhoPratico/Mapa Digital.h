28c4a547ecd108325f14f4b3e7ad6d5ad6bab7f7
Joao Rui Fontes <joao.rui.fontes@gmail.com>
2013-11-20 15:42:34 +0000

    Inserção de metodos header na classe mapa digital

--- TrabalhoPratico/Mapa Digital.h
+++ TrabalhoPratico/Mapa Digital.h
@@ -6,7 +6,17 @@
 #include "graphStlPath.h"
 
 class MapaDigital:graphStlPath <LocTurist*,ViaLig*>{
-
-}
+private:
+public:
+	MapaDigital();
+	MapaDigital(const MapaDigital& md);
+	
+	
+	void construirGrafo(const vector<LocTurist*>& lt,const vector<ViaLig*>& vl);
+	//determinar a complexidade temporal
+	void percursosPossiveis(const string orig,const string dest);
+	void percursosPossiveis(const LocTurist& orig,const LocTurist& dest);
+	void percursosPossiveis(const string orig,const LocTurist
+};
 
 #endif // !
